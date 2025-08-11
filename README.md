532ff67299934e06cb4b4aa54e10176510f9e849
Pabitra1230 <kumarpabitra611@gmail.com>
2025-08-11 20:39:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# basic-calculator-in-C
+A basic calculator in C that can add, subtract, multiply, and divide two numbers. Useing switch-case for operations and the program handles division by zero gracefully.
---
+++ calcul.cpp
@@ -0,0 +1,42 @@
+ #include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op); // Notice the space before %c to skip whitespace
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error: Division by zero is not allowed.\n");
+            } else {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+    }
+
+    return 0;
+}
